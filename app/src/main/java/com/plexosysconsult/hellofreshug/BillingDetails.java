677c8db0e9d54320e55817fb9ed2a86099271f72
eriqsenyonga <senyonga@gmail.com>
2016-08-07 15:37:35 +0300

    work in progress billingactvity

--- app/src/main/java/com/plexosysconsult/hellofreshug/BillingDetails.java
+++ app/src/main/java/com/plexosysconsult/hellofreshug/BillingDetails.java
@@ -3,11 +3,19 @@ package com.plexosysconsult.hellofreshug;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 
 public class BillingDetails extends AppCompatActivity {
+
+    TextInputLayout tilFirstName, tilSurName, tilEmail, tilPhoneNumber, tilPassword, tilReenterPassword, tilDeliveryAddress;
+    Button bPlaceOrder;
+    CheckBox cbCreateAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +26,18 @@ public class BillingDetails extends AppCompatActivity {
 
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        tilFirstName = (TextInputLayout) findViewById(R.id.til_firstname);
+        tilSurName = (TextInputLayout) findViewById(R.id.til_surname);
+        tilEmail = (TextInputLayout) findViewById(R.id.til_email);
+        tilPhoneNumber = (TextInputLayout) findViewById(R.id.til_phone_number);
+        tilDeliveryAddress = (TextInputLayout) findViewById(R.id.til_address);
+        tilPassword = (TextInputLayout) findViewById(R.id.til_password);
+        tilReenterPassword = (TextInputLayout) findViewById(R.id.til_reenter_password);
+        bPlaceOrder = (Button) findViewById(R.id.b_place_order);
+        cbCreateAccount = (CheckBox) findViewById(R.id.cb_create_account);
+
+
     }
 
 }
