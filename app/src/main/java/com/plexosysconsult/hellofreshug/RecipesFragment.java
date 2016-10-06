b9d59c8cc76562065a74b8bf80e4f1e8203c18ab
eriqsenyonga <senyonga@gmail.com>
2016-10-06 17:18:25 +0300

    pull down of Recipes now successful. Next to put them in RecyclerView and make them clickable

--- app/src/main/java/com/plexosysconsult/hellofreshug/RecipesFragment.java
+++ app/src/main/java/com/plexosysconsult/hellofreshug/RecipesFragment.java
@@ -2,16 +2,44 @@ package com.plexosysconsult.hellofreshug;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class RecipesFragment extends Fragment {
+
+
+    RecyclerView rvRecipes;
+    String URL_GET_RECIPES = "http://www.hellofreshuganda.com/api/get_posts/?post_type=recipe";
+    MyApplicationClass myApplicationClass = MyApplicationClass.getInstance();
+    List<Recipe> recipeList;
+    ProgressBar pbLoading;
+    View v;
 
 
     public RecipesFragment() {
@@ -23,7 +51,72 @@ public class RecipesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recipes, container, false);
+
+        v = inflater.inflate(R.layout.fragment_recipes, container, false);
+
+        rvRecipes = (RecyclerView) v.findViewById(R.id.recycler_view);
+
+        pbLoading = (ProgressBar) v.findViewById(R.id.pb_loading);
+
+        return v;
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+
+        rvRecipes.hasFixedSize();
+        rvRecipes.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+
+        recipeList = new ArrayList();
+
+
+fetchFruitsJson();
+    }
+
+    private void fetchFruitsJson() {
+
+        StringRequest recipeRequest = new StringRequest(Request.Method.POST, URL_GET_RECIPES,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        try {
+                            Log.d("recipes", response);
+                            JSONObject jsonResponse = new JSONObject(response);
+                           // putJsonIntoList(jsonResponse);
+                            pbLoading.setVisibility(View.GONE);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+//                        Toast.makeText(getActivity(), error.toString(), Toast.LENGTH_LONG).show();
+                        pbLoading.setVisibility(View.GONE);
+
+                    }
+                }) {
+
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> map = new HashMap<>();
+                map.put("category", "recipe");
+
+                return map;
+            }
+        };
+
+
+        myApplicationClass.add(recipeRequest);
+
+    }
 }
